c42bee202af76f692c8b798cd0a23f4a7d88a1c4
Aaron Sheoran <aaronsheoran@gmail.com>
2020-11-27 18:24:12 -0800

    fix pr feedback

--- game.js
+++ game.js
@@ -1,6 +1,9 @@
-let canvas = document.getElementById("gameCanvas")
-let context = canvas.getContext("2d")
+function loadGame() {
+    let canvas = document.getElementById("gameCanvas")
 
+    let context = canvas.getContext("2d")
+
+//defining the player
 let player = {
     height: 32,
     width: 32,
@@ -11,6 +14,7 @@ let player = {
     velocityY:0
 }
 
+//controller logic
 let controller = {
     left:false,
     right:false,
@@ -32,4 +36,59 @@ let controller = {
             controller.right = keyState
         }
     }
+}
+
+function loop(){
+    if(controller.up&&!player.isJumping){
+        player.isJumping=true
+        player.velocityY-=20
+    }
+    if(controller.left){
+        player.velocityX-=0.5
+    }
+    if(controller.right){
+        console.log(player.velocityX)
+        player.velocityX+=0.5
+    }
+
+    player.velocityY+=1.5
+    player.x+=player.velocityX
+    player.y+=player.velocityY
+    player.velocityX*=0.9
+    player.velocityY*=0.9
+
+    if(player.y>338){
+        player.isJumping=false
+        player.y=338
+        player.velocityY=0
+    }
+    if(player.x<-20){
+        player.x=1220
+    }else if(player.x>1220){
+        player.x=-20
+    }
+
+    // Creates the backdrop for each frame  
+    context.fillStyle = "#201A23";  
+    context.fillRect(0, 0, 1220, 400); // x, y, width, height
+  // Creates and fills the cube for each frame  
+  context.fillStyle = "#8DAA9D"; // hex for cube color 
+  context.beginPath();  
+  context.rect(player.x, player.y, player.width, player.height);  
+  context.fill();
+  // Creates the "ground" for each frame  
+  context.strokeStyle = "#2E2532";  
+  context.lineWidth = 30;  
+  context.beginPath();  
+  context.moveTo(0, 385);  
+  context.lineTo(1220, 385);  
+  context.stroke();
+
+  window.requestAnimationFrame(loop)
+}
+
+window.addEventListener("keydown", controller.listener)
+window.addEventListener("keyup", controller.listener)
+window.requestAnimationFrame(loop)
+
 }
